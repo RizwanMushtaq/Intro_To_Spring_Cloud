0742e4a1122097bc02799873b5886adc176a68ee
Rizwan <rizwanmushtaq15@gmail.com>
2025-10-07 19:09:51 +0200

    feat: updated yml file

--- finish/greeting/greeting/src/main/java/com/example/greeting/GreetingProperties.java
+++ finish/greeting/greeting/src/main/java/com/example/greeting/GreetingProperties.java
@@ -3,7 +3,6 @@ package com.example.greeting;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties()
 public class GreetingProperties {
