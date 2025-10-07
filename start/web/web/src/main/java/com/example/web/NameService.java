42d9690564583fd83815f3d26e92bfafba6f1d51
Rizwan <rizwanmushtaq15@gmail.com>
2025-10-07 15:18:36 +0200

    feat: added Web App as another service

---
+++ start/web/web/src/main/java/com/example/web/NameService.java
@@ -0,0 +1,15 @@
+package com.example.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NameService {
+  private static final String URL = "http://localhost:7070";
+  private RestTemplate rest = AppConfig.restTemplate();
+
+  public String getName() {
+    return rest.getForObject(URL, String.class);
+  }
+}
