cb727dcf75067626275cc0c28e1135c467c1b57b
Rizwan <rizwanmushtaq15@gmail.com>
2025-10-07 19:14:22 +0200

    feat: updated name app

--- finish/name/name/src/main/java/com/example/name/NameApplication.java
+++ finish/name/name/src/main/java/com/example/name/NameApplication.java
@@ -1,20 +1,11 @@
 package com.example.name;
 
-import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class NameApplication {
   public static void main(String[] args) {
     SpringApplication.run(NameApplication.class, args);
-  }
-
-  @Bean
-  public Sampler getSampler() {
-    return Sampler.ALWAYS_SAMPLE;
   }
 }
