ed7673633a513665a52b055b3133ab8b9be0ca07
Rizwan <rizwanmushtaq15@gmail.com>
2025-10-07 17:05:11 +0200

    feat: added spring cloud dependencies

--- finish/greeting/greeting/src/main/java/com/example/greeting/GreetingApplication.java
+++ finish/greeting/greeting/src/main/java/com/example/greeting/GreetingApplication.java
@@ -1,13 +1,22 @@
 package com.example.greeting;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GreetingProperties.class)
+@EnableDiscoveryClient
 public class GreetingApplication {
   public static void main(String[] args) {
     SpringApplication.run(GreetingApplication.class, args);
+  }
+
+  @Bean
+  public Sampler getSampler() {
+    return Sampler.ALWAYS_SAMPLE;
   }
 }
